8359c2dee2ad454e33fa72ecd61c9f074c30cb07
Christopher Hiller <chiller@decipherinc.com>
2014-08-07 22:20:36 -0700

    fixing release task which should be building

--- tasks/release.js
+++ tasks/release.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('release', 'Build & Bump', function releaseTask(target) {
     grunt.task.run('bump-only:' + target);
+    grunt.task.run('build');
     grunt.task.run('docs');
     grunt.task.run('gh-pages');
     grunt.task.run('bump-commit');
